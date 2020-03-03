9860d94318060471f9762c24c69f23d810625321
oscar_9414 <oscar9414ps@gmail.com>
2020-03-03 21:14:56 +0100

    Leyendo la información desde el JSON con la interfaz

---
+++ OscarPortafolio/src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    equipo_trabajo?: any[];
+}
